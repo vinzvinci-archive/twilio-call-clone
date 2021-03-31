82b962abbd714ea98deaba79e6dddc8db002784a
Vincent Villafuerte <73097560+vinzvinci@users.noreply.github.com>
2021-03-31 20:02:32 +0800

    add capability-token

--- assets/js/quickstart.js
+++ assets/js/quickstart.js
@@ -8,7 +8,7 @@
   var device;
 
   log('Requesting Capability Token...');
-  $.getJSON('https://blue-chamois-4051.twil.io/client-voice')
+  $.getJSON('https://blue-chamois-4051.twil.io/capability-token')
     .then(function (data) {
       log('Got a token.');
       console.log('Token: ' + data.token);
